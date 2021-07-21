234c20cd44e1066abf0d9cb70ec72621ef91342b
Pete Kriengsiri <pkriengsiri@banyancom.com>
2021-07-21 18:21:43 -0400

    Updated shopping-list and shopping-edit components

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,5 +1,5 @@
 export class Ingredient {
-    constructor (public name: string, public quantity: string) {
+    constructor (public name: string, public quantity: number) {
         this.name = name;
         this.quantity = quantity;
     }
