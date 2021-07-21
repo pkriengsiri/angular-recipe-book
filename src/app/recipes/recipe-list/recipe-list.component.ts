e039edcfde02b7e5e18a16b6be359e5c447452f4
Pete Kriengsiri <pkriengsiri@banyancom.com>
2021-07-21 17:03:52 -0400

    Added to recipe detail, added ingredient model, added to shopping-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from "../recipe.model"
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Test recipe description','https://live.staticflickr.com/3186/2284439692_d9b92346cf_b.jpg')
+    new Recipe(
+      'A test recipe',
+      'Test recipe description',
+      'https://live.staticflickr.com/3186/2284439692_d9b92346cf_b.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'Test recipe description',
+      'https://live.staticflickr.com/3186/2284439692_d9b92346cf_b.jpg'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+    constructor (public name: string, public quantity: string) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
